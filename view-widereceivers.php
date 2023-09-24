5b2d0668854d6573bacab80baf70ee22bf52b31d
Parker Williams <111520404+parkwillz@users.noreply.github.com>
2023-09-24 17:20:58 -0500

    Update view-widereceivers.php

--- view-widereceivers.php
+++ view-widereceivers.php
@@ -1,1 +1,25 @@
-
+<H1>Wide Receivers</H1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>ADOT</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while($widereceiver = $widereceivers->fetch_assoc())  {
+?>
+  <tr>
+    <td><?php echo $widereceivers['widereceiver_id']; ?> </td>
+    <td><?php echo $widereceivers['widereceiver_name']; ?></td>
+    <td><?php echo $widereceivers['widereceiver_adot']; ?></td>
+  </tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
