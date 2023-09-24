8138c5c5688dfa6cb9a95e496408a5a2c38015ab
Parker Williams <111520404+parkwillz@users.noreply.github.com>
2023-09-24 17:17:00 -0500

    Update widereceivers.php

--- widereceivers.php
+++ widereceivers.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-widereceivers.php");
 
+$pageTitle = "Wide Receivers";
+include "view-header.php";
+$widereceivers = selectWidereceivers();
+include "view-widereceivers.php";
+include "view-footer.php";
+?>
