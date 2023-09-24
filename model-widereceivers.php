86f2a087c281e4345367a21c3415a1358417aaa8
Parker Williams <111520404+parkwillz@users.noreply.github.com>
2023-09-24 17:22:43 -0500

    Update model-widereceivers.php

--- model-widereceivers.php
+++ model-widereceivers.php
@@ -2,7 +2,7 @@
 function selectWidereceivers() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("select widereceiver_id, widereceiver_name, widereceiver_adot from 'widereceiver'");
+        $stmt = $conn->prepare("SELECT widereceiver_id, widereceiver_name, widereceiver_adot FROM 'widereceiver'");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
