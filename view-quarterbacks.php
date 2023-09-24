87a9145f97ea91cbe8c905ef835a010bcc03ed1d
Parker Williams <111520404+parkwillz@users.noreply.github.com>
2023-09-24 17:09:17 -0500

    Update view-quarterbacks.php

--- view-quarterbacks.php
+++ view-quarterbacks.php
@@ -1,1 +1,25 @@
-
+<H1>Quarterbacks</H1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Air Yards/Attempt</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while($quarterback = $quarterbacks->fetch_assoc())  {
+?>
+  <tr>
+    <td><?php echo $quarterback['quarterback_id']; ?> </td>
+    <td><?php echo $quarterback['quarterback_name']; ?></td>
+    <td><?php echo $quarterback['quarterback_adot']; ?></td>
+  </tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
