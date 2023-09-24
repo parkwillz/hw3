98cb000c97757d453eacf28554c5a8956a6fc3be
Parker Williams <111520404+parkwillz@users.noreply.github.com>
2023-09-24 18:53:24 -0500

    Update wr-qb.php

--- wr-qb.php
+++ wr-qb.php
@@ -4,7 +4,7 @@ require_once("model-wr-qb.php");
 
 $pageTitle = "QBs by WR";
 include "view-header.php";
-$wrqbs = selectWRQB($_GET['id']);
+$wrqbs = selectWRQB($_GET['wrqb']);
 include "view-wr-qb.php";
 include "view-footer.php";
 ?>
