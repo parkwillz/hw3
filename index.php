9d1b5162b2e1ab9ad9990f9dc57f794ba4c9ffb9
Parker Williams <111520404+parkwillz@users.noreply.github.com>
2023-09-25 17:19:15 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,12 @@ $pageTitle = "Home";
 include "view-header.php";
 ?>
 <h1>HW3</h1>
+<p>This website displays correlation between Air Yards/Attempt by a QB and ADOT from a teams WR1 and WR2</p>
+<p>When you click on the quarterbacks tab you will see each qb ranked highest to lowest on air yards/attempt</p>
+<p>The wide receivers page displays the WR1 and WR2 per each NFL teams ADOT</p>
+<p></p>
+<p>Air Yards/Attempt is meant to show how far a QB throws the ball in the air before the ball is caught by their primary target</p>
+<p>ADOT is the average depth of target for a Wide Receiver. For example, if Tyreek Hill's ADOT is a 14.8, he is targeted an average of 14.8 yards down the field</p>
 <?php
 include "view-footer.php";
 ?>
