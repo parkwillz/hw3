998c3df8f435159046ab0b30faeddb7a2802be73
Parker Williams <111520404+parkwillz@users.noreply.github.com>
2023-09-25 10:49:09 -0500

    Update model-qbs-with-wrs.php

--- model-qbs-with-wrs.php
+++ model-qbs-with-wrs.php
@@ -12,4 +12,19 @@ function selectQuarterbacks() {
         throw $e;
     }
 }
+
+function selectWRQB($wrid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("select q.quarterback_id, q.quarterback_name, q.quarterback_adot, t.teamname from quarterback q join team t on q.quarterback_id = t.quarterback_id where t.widereceiver_id=?");
+         $stmt->bind_param("i", $wrid);  
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
