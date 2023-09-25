986ed45a6fd0d88ce8a1a3cdff891e5601e045e0
Parker Williams <111520404+parkwillz@users.noreply.github.com>
2023-09-25 11:05:12 -0500

    Update view-qbs-with-wrs.php

--- view-qbs-with-wrs.php
+++ view-qbs-with-wrs.php
@@ -8,13 +8,16 @@ while($quarterback = $quarterbacks->fetch_assoc())  {
     <div class="card-body">
       <h5 class="card-title"><?php echo $quarterback['quarterback_name']; ?></h5>
       <p class="card-text">
+      <ul class="list-group">
 <?php
-  $wideouts = selectWideoutsByQB($quarterback['quarterback_id'];);
-  while ($wideouts->fetch_assoc()){
+  $qbwr = selectWideoutsByQB($quarterback['quarterback_id'];);
+  while ($qbwr->fetch_assoc()){
 ?>
+    <li class="list-group-item"><?php echo $qbwr['widereceiver_name']; ?> - <?php echo $qbwr['widereceiver_adot']; ?></li>
 <?php
   }
 ?>
+      </ul>
       </p>
       <p class="card-text"><small class="text-body-secondary">Air Yards: <?php echo $quarterback['quarterback_adot']; ?></small></p>
     </div>
