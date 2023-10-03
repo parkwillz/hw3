c92d41a8a87a703f0ebb2a12f68bff058572d503
Parker Williams <111520404+parkwillz@users.noreply.github.com>
2023-10-03 12:49:44 -0500

    Update view-widereceivers.php

--- view-widereceivers.php
+++ view-widereceivers.php
@@ -1,6 +1,6 @@
 <H1>Wide Receivers</H1>
 <div class="table-responsive">
-  <table class="table table-striped table-hove">
+  <table class="table table-striped table-hover">
     <thead>
       <tr>
       <th>ID</th>
