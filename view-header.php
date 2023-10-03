84e031b420a7ba528f572b07fedacc52fd6bbb20
Parker Williams <111520404+parkwillz@users.noreply.github.com>
2023-10-03 12:41:19 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -20,7 +20,7 @@
   </head>
   <body>
     <div class ="container">
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <nav class="navbar navbar-expand-lg bg-info-dark">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
