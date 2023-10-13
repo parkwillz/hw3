0895e200f6868a3e0e69b4224bef8fba14c301bf
Parker Williams <111520404+parkwillz@users.noreply.github.com>
2023-10-13 12:53:04 -0500

    Update model-quarterbacks.php

--- model-quarterbacks.php
+++ model-quarterbacks.php
@@ -12,4 +12,46 @@ function selectQuarterbacks() {
         throw $e;
     }
 }
+
+function insertQuarterbacks($qName, $qADOT) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO 'quarterback' ('quarterback_name', 'quarterback_adot') VALUES (?, ?)");
+        $stmt->bind_param("ss", $qName, $qADOT);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateQuarterbacks($qName, $qADOT, $qbid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update 'quarterback' set 'quarterback_name' = ?, 'quarterback_adot' = ?) where quarterback_id = ?");
+        $stmt->bind_param("ssi", $qName, $qADOT, $qbid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteQuarterbacks($qbid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from 'quarterback' where quarterback_id=?");
+        $stmt->bind_param("i", $qbid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
