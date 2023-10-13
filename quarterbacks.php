ac4d32313be7fa5013abfb40a139075acb3622a7
Parker Williams <111520404+parkwillz@users.noreply.github.com>
2023-10-13 13:29:16 -0500

    Update quarterbacks.php

--- quarterbacks.php
+++ quarterbacks.php
@@ -9,7 +9,7 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      insertQuarterbacks($_POST['quarterbackName'], $_POST['quarterbackADOT']);
+      insertQuarterbacks($_POST['qName'], $_POST['qADOT']);
       break;
   }
 }
