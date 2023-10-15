acd3a7c42218c05573281d90faa84d5f223cbd6a
Parker Williams <111520404+parkwillz@users.noreply.github.com>
2023-10-15 14:17:36 -0500

    Update model-runningbacks.php

--- model-runningbacks.php
+++ model-runningbacks.php
@@ -12,4 +12,46 @@ function selectRunningbacks() {
         throw $e;
     }
 }
+
+function insertRunningbacks($rName, $rRYOA) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO runningback (runningback_name, runningback_ryoa) VALUES (?, ?)");
+        $stmt->bind_param("ss", $rName, $rRYOA);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateRunningbacks($rName, $rARYOA, $rbid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update runningback set runningback_name = ?, runningback_RYOA = ? where runningback_id = ?");
+        $stmt->bind_param("ssi", $rName, $rRYOA, $rbid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteQuarterbacks($rbid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from runningback where runningback_id=?");
+        $stmt->bind_param("i", $rbid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
