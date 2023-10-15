c054501297eb15bf9d9863e3713ac0c92af682d0
Parker Williams <111520404+parkwillz@users.noreply.github.com>
2023-10-15 13:37:35 -0500

    Update model-widereceivers.php

--- model-widereceivers.php
+++ model-widereceivers.php
@@ -12,4 +12,46 @@ function selectWidereceivers() {
         throw $e;
     }
 }
+
+function insertWidereceivers($wName, $wADOT) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO widereceiver (widereceiver_name, widereceiver_adot) VALUES (?, ?)");
+        $stmt->bind_param("ss", $wName, $wADOT);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateWidereceivers($wName, $wADOT, $wrid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update widereceiver set widereceiver_name = ?, widereceiver_adot = ? where widereceiver_id = ?");
+        $stmt->bind_param("ssi", $wName, $wADOT, $wrid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteWidereceivers($wrid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from widereceiver where widereceiver_id=?");
+        $stmt->bind_param("i", $wrid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
