d66b49016defaee088fba73dc849fa239ce812cc
Parker Williams <111520404+parkwillz@users.noreply.github.com>
2023-10-15 13:39:08 -0500

    Update widereceivers.php

--- widereceivers.php
+++ widereceivers.php
@@ -2,6 +2,33 @@
 require_once("util-db.php");
 require_once("model-widereceivers.php");
 
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertWidereceivers($_POST['wName'], $_POST['wADOT'])) {
+        echo '<div class="alert alert-success" role="alert">Widereceiver Added!</div>';
+      } else {
+        echo '<div class="alert alert-warning" role="alert">Error</div>';
+      }
+      break;
+    case "Edit":
+      if (updateWidereceivers($_POST['wName'], $_POST['wADOT'], $_POST['wrid'])) {
+        echo '<div class="alert alert-success" role="alert">Widereceiver Edited!</div>';
+      } else {
+        echo '<div class="alert alert-warning" role="alert">Error</div>';
+      }
+      break;
+    case "Delete":
+      if (deleteWidereceivers($_POST['wrid'])) {
+        echo '<div class="alert alert-success" role="alert">Widereceiver Deleted!</div>';
+      } else {
+        echo '<div class="alert alert-warning" role="alert">Error</div>';
+      }
+      break;
+  }
+}
+
+
 $pageTitle = "Wide Receivers";
 include "view-header.php";
 $widereceivers = selectWidereceivers();
