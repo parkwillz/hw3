942f7231ca2e6baa56c6f0fd778a3b6b7d551490
Parker Williams <111520404+parkwillz@users.noreply.github.com>
2023-10-15 14:19:06 -0500

    Update runningbacks.php

--- runningbacks.php
+++ runningbacks.php
@@ -4,6 +4,33 @@ require_once("model-runningbacks.php");
 
 $pageTitle = "Runningbacks";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertRunningbacks($_POST['rName'], $_POST['rRYOA'])) {
+        echo '<div class="alert alert-success" role="alert">Runningback Added!</div>';
+      } else {
+        echo '<div class="alert alert-warning" role="alert">Error</div>';
+      }
+      break;
+    case "Edit":
+      if (updateRunningbacks($_POST['rName'], $_POST['rRYOA'], $_POST['rbid'])) {
+        echo '<div class="alert alert-success" role="alert">Runningback Edited!</div>';
+      } else {
+        echo '<div class="alert alert-warning" role="alert">Error</div>';
+      }
+      break;
+    case "Delete":
+      if (deleteRunningbacks($_POST['rbid'])) {
+        echo '<div class="alert alert-success" role="alert">Runningback Deleted!</div>';
+      } else {
+        echo '<div class="alert alert-warning" role="alert">Error</div>';
+      }
+      break;
+  }
+}
+
 $runningbacks = selectRunningbacks();
 include "view-runningbacks.php";
 include "view-footer.php";
