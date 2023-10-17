955c45ff1f847761b300687793ef909f19df1d23
Parker Williams <111520404+parkwillz@users.noreply.github.com>
2023-10-16 21:05:13 -0500

    Update view-qbs-with-wrs.php

--- view-qbs-with-wrs.php
+++ view-qbs-with-wrs.php
@@ -1,31 +1,3 @@
-<H1>Quarterbacks With WideReceivers</H1>
-<div class="card-group">
-  
-<?php
-while($quarterback = $quarterbacks->fetch_assoc())  {
-?>
-  <div class="card">
-    <div class="card-body">
-      <h5 class="card-title"><?php echo $quarterback['quarterback_name']; ?></h5>
-      <p class="card-text">
-      <ul class="list-group">
-<?php
-  $qbwrs = selectQBWR($quarterback['quarterback_id']);
-  while ($qbwr = $qbwrs->fetch_assoc()){
-?>
-    <li class="list-group-item"><?php echo $qbwr['widereceiver_name']; ?> - <?php echo $qbwr['widereceiver_adot']; ?></li>
-<?php
-  }
-?>
-      </ul>
-      </p>
-      <p class="card-text"><small class="text-body-secondary">Air Yards: <?php echo $quarterback['quarterback_adot']; ?></small></p>
-    </div>
-<?php
-}
-?>
-</div>
-
 <div class="row">
   <div class="col">
 <h1>Quarterbacks with Wide Receivers</h1>
