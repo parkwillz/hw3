6ffd95e33ddb1eafcf2c19d89d2f4a762d218739
Parker Williams <111520404+parkwillz@users.noreply.github.com>
2023-10-16 22:22:43 -0500

    Update qbs-with-wrs.php

--- qbs-with-wrs.php
+++ qbs-with-wrs.php
@@ -9,21 +9,21 @@ if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
       if (insertTeam($_POST['qbid'], $_POST['wrid'], $_POST['wName'], $_POST['wADOT'])) {
-        echo '<div class="alert alert-success" role="alert">Section added.</div>';
+        echo '<div class="alert alert-success" role="alert">Team added.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
       break;
     case "Edit":
       if (updateTeam($_POST['qbid'], $_POST['wrid'], $_POST['wName'], $_POST['wADOT'], $_POST['tid'])) {
-        echo '<div class="alert alert-success" role="alert">Section edited.</div>';
+        echo '<div class="alert alert-success" role="alert">Team edited.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
       break;
     case "Delete":
       if (deleteTeam($_POST['tid'])) {
-        echo '<div class="alert alert-success" role="alert">Section deleted.</div>';
+        echo '<div class="alert alert-success" role="alert">Team deleted.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
