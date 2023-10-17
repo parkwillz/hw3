613feee75a4f73f6779b8c24d4f151813c7d89d5
Parker Williams <111520404+parkwillz@users.noreply.github.com>
2023-10-16 21:23:36 -0500

    Update view-qbs-with-wrs-editform.php

--- view-qbs-with-wrs-editform.php
+++ view-qbs-with-wrs-editform.php
@@ -11,7 +11,7 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="editSectionModalLabel<?php echo $qbwr['team_id']; ?>">Edit Quarterback</h1>
+        <h1 class="modal-title fs-5" id="editTeamModalLabel<?php echo $qbwr['team_id']; ?>">Edit Quarterback</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
