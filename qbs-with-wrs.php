9a070718d2791e45341741d05fcc8e4b45624760
Parker Williams <111520404+parkwillz@users.noreply.github.com>
2023-10-16 20:40:26 -0500

    Update qbs-with-wrs.php

--- qbs-with-wrs.php
+++ qbs-with-wrs.php
@@ -4,6 +4,34 @@ require_once("model-qbs-with-wrs.php");
 
 $pageTitle = "Quarterbacks With Wide Receivers";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertTeam($_POST['qbid'], $_POST['wrid'], $_POST['wName'], $_POST['wADOT'])) {
+        echo '<div class="alert alert-success" role="alert">Section added.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    case "Edit":
+      if (updateTeam($_POST['qbid'], $_POST['wrid'], $_POST['wName'], $_POST['wADOT'], $_POST['tid'])) {
+        echo '<div class="alert alert-success" role="alert">Section edited.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    case "Delete":
+      if (deleteTeam($_POST['tid'])) {
+        echo '<div class="alert alert-success" role="alert">Section deleted.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+  }
+}
+
+
 $quarterbacks = selectQuarterbacks();
 include "view-qbs-with-wrs.php";
 include "view-footer.php";
