1182fccd747af5a5d10e3fe8d7a2a2f6756cf234
Parker Williams <111520404+parkwillz@users.noreply.github.com>
2023-10-16 19:46:11 -0500

    Update view-quarterback-input-list.php

--- view-quarterback-input-list.php
+++ view-quarterback-input-list.php
@@ -1,1 +1,13 @@
-
+<select class="form-select" id="qbid" name="qbid">
+<?php
+while ($quarterbacmItem = $quarterbackList->fetch_assoc()) {
+  $selText = "";
+  if ($selectedQuarterback == $quarterbackItem['quarterback_id']) {
+    $selText = " selected";
+  }
+?>
+  <option value="<?php echo $quarterbackItem['quarterback_id']; ?>"<?=$selText?>><?php echo $quarterbackItem['quarterback_name']; ?></option>
+<?php
+}
+?>
+</select>
