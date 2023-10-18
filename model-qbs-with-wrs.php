b8a7897c31c005e837cf194a6890cd1f86004489
Parker Williams <111520404+parkwillz@users.noreply.github.com>
2023-10-18 13:50:30 -0500

    Update model-qbs-with-wrs.php

--- model-qbs-with-wrs.php
+++ model-qbs-with-wrs.php
@@ -16,7 +16,7 @@ function selectQuarterbacks() {
 function selectQBWR($qbid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("select w.widereceiver_id, w.widereceiver_name, w.widereceiver_adot, t.teamname from widereceiver w join team t on w.widereceiver_id = t.widereceiver_id where t.quarterback_id=?");
+        $stmt = $conn->prepare("select t.team_id, w.widereceiver_id, w.widereceiver_name, w.widereceiver_adot, t.teamname from widereceiver w join team t on w.widereceiver_id = t.widereceiver_id where t.quarterback_id=?");
          $stmt->bind_param("i", $qbid);  
         $stmt->execute();
         $result = $stmt->get_result();
